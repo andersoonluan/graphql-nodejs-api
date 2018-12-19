792cfd03bc3ce83590773a76024d2204c0ff3590
Anderson Rodrigues <anderson.rodrigues@gruporbs.com.br>
2018-12-19 12:48:36 -0200

    include config cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
 import * as graphqlHTTP from 'express-graphql';
+import * as cors from 'cors';
 
 import db from './models/index';
 import schema from './graphql/schema';
@@ -27,6 +28,15 @@ class App {
 
     // Middleware to sincronization GraphQL
     private middleware() : void {
+
+        // Config CORS
+        this.express.use(cors({
+            origin: '*',
+            methods: ['GET','POST'],
+            allowedHeaders: ['Content-Type', 'Authorization', 'Accept-Encoding'],
+            preflightContinue: false,
+            optionsSuccessStatus: 204
+        }));
 
        this.express.use('/graphql', 
        
